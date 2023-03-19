cc74f0728f8f7dfb4052e27f302d4df8e5c19f4d
Sunmaila <aderopo1995@gmail.com>
2023-03-19 09:59:31 -0700

    my commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Description: 'print possible combination 0f single-digit number'
+ * Description: 'print possible combination 0f single-digit numbers'
  * Return: Always 0
  */
 int main(void)

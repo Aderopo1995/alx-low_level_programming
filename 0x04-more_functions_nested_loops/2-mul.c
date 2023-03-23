7b8e3155484023d3fcacfaf386a20aa211108d3f
Sunmaila <aderopo1995@gmail.com>
2023-03-23 12:14:53 -0700

    commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,10 +3,10 @@
  * mul - a function that multiplies two integers.
  * @a: An input integer
  * @b: An input integer
- * Return: Always 0
+ * Result: Always 0
  *
  * Return: the multiplication of the two integers
- */
+*/
 int mul(int a, int b)
 {
 return (a * b);

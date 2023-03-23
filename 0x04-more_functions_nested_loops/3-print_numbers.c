649cb54b9bf7f7488d737715e75539df1b094375
Sunmaila <aderopo1995@gmail.com>
2023-03-23 12:51:49 -0700

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,18 @@
 #include "main.h"
+
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9,
- * followed by a new line.
- * Result: Always 0
- *
+ * print_numbers - print 0123456789
+ * code by prince Solomon
  * Return: void
  */
+
 void print_numbers(void)
 {
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
